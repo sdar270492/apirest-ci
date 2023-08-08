aede7b6f404804c8bfbb2b45282aceefe3d45104
Sergio Aguilar <sergioaguilar.ti@outlook.com>
2023-08-07 22:53:16 -0500

    Initial commit

---
+++ app/Controllers/Clientes.php
@@ -0,0 +1,11 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Clientes extends Controller
+{
+    public function index() 
+    {
+        echo 'Hola Clientes!';
+    }
+}
---
+++ app/Models/ClientesModel.php
@@ -0,0 +1,8 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ClientesModel extends Model
+{
+    protected $table = 'clientes';
+}
